573343207e84df98d295889da838b73079dd1ce7
Collin Argo <cargo@insiten.com>
2021-01-13 21:49:07 -0500

    fix import

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
-import path from 'path';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: path.join(__dirname, '../../client', 'build'),
+      rootPath: join(__dirname, '../../client', 'build'),
       serveRoot: '/',
     }),
   ],
