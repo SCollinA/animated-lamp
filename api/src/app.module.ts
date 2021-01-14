dcf80cdc32f134033651a6dc41cabc2d4b9a6700
Collin Argo <cargo@insiten.com>
2021-01-13 21:22:36 -0500

    Add package-lock.jsons and serve static

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '../../client', 'build'),
+      serveRoot: '/',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
